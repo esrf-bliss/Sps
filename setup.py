ec3b83362a5e16636b060e0619865756b40e1a80
seb <seb@e4a84942-a05b-4821-847d-85a52f60f39f>
2009-04-02 11:44:51 +0000

    remove sps_lut.c modify setup.py to remove sps_lut
    git-svn-id: https://scm.blissgarden.org/svn/sps/trunk@11 e4a84942-a05b-4821-847d-85a52f60f39f

--- setup.py
+++ setup.py
@@ -24,31 +24,25 @@ elif platform.system() == 'SunOS' :
 else:
     extra_compile_args = []
     
-ext_modules = [Extension(
-                        name = 'spslut',
-                        sources=['Src/sps_lut.c',
-                                 'Src/spslut_py.c'],
-                        extra_compile_args = extra_compile_args,
-                        include_dirs  = ['Include', numpy.get_include()],
-                   )]
 if sys.platform == "win32":
     define_macros = [('WIN32',None)]
 else:
     define_macros = []
-    ext_modules.append( Extension(
-                            name = 'sps',
-                            sources=['Src/sps.c',
-                                     'Src/sps_py.c'],
-                            extra_compile_args = extra_compile_args,
-                            include_dirs  = ['Include', numpy.get_include()]))
-setup (
+    ext_modules = [Extension(
+        name = 'sps',
+        sources=['Src/sps.c',
+                 'Src/sps_py.c'],
+        extra_compile_args = extra_compile_args,
+        include_dirs  = ['Include', numpy.get_include()])]
+    setup (
         name         = "sps",
         version      = "1.0",
         description  = "shared memory and spec",
         author       = "BLISS Group",
         author_email = "rey@esrf.fr",
         url          = "http://www.esrf.fr/computing/bliss/",
-
+        
         # Description of the modules and packages in the distribution
         #extra_path   = 'Pybliss',
         ext_modules  = ext_modules)
+    
